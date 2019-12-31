3acdf074485f2f5296b45a1f715bad47e4ec7ce6
bosmanpa <bsmn.ptrck@gmail.com>
2019-12-30 21:07:04 -0600

    find or create by user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find(params[:id])
-        render json: user
+        user = User.find_or_create_by(params[:username])
+        render json: user, include: :notes
     end
 
 end
